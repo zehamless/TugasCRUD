c3aaeb875acc57982ef024fe545fcf207ccb5aa8
petersonhz25 <82883040+zehamless@users.noreply.github.com>
2023-03-19 20:54:53 +0700

    fixup! crud category done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::with('categories')->get();
+        $products = Product::with('category')->get();
         return view('home.products.product', compact('products'));
     }
 
